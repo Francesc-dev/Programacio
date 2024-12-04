168489f11b5a56601aefdb1db0fbf1564dd37c27
Folivera <folivera@cendrassos.net>
2024-12-04 16:34:45 +0100

    pruebas

---
+++ Tascas/Tasca0/Client.cs
@@ -0,0 +1,19 @@
+namespace Tasca0;
+
+public class Client
+{
+    public Client(string nomClient)
+    {
+        Nom = nomClient;
+        Saldo = 0;
+    }
+    public string Nom { get; }
+
+    // private saldo;
+    // public getSaldo() { return saldo; }
+    // public setSaldo(int x) { saldo = x; }
+    
+    public int Saldo { get; set; }
+    
+    
+}
