f03e992fbb5d70a242543246a01fe7314637402d
Folivera <folivera@cendrassos.net>
2024-12-06 13:06:07 +0100

    falta terminar

--- Tascas/Tasca_0/Program.cs
+++ Tascas/Tasca_0/Program.cs
@@ -1,19 +1,51 @@
-﻿namespace Tasca_0;
+﻿using System.Linq.Expressions;
+
+namespace Tasca_0;
 
 class Program
 {
     static void Main(string[] args)
     {
         var banquer = new Banker();
-        
-        banquer.CreaClient("Frederic");
-        
-        Console.WriteLine(banquer.PosarDiners("Frederic", 100));
-        Console.WriteLine(banquer.TreureDiners("Frederic", 200));
-        Console.WriteLine(banquer.TreureDiners("Frederic", 10));
-        
-        Console.WriteLine(banquer.TreureDiners("Manel", 50));
-        banquer.CreaClient("Manel");
-        Console.WriteLine(banquer.PosarDiners("Manel", 5));
+
+        Console.WriteLine("¿Que quieres hacer?");
+        Console.WriteLine("Crear (1), Mover (2) o Eliminar (3)");
+        string Leer = Console.ReadLine();
+
+        switch (Leer)
+        {
+            case "1":
+                Console.WriteLine("Ingrese el nuevo nombre del usuario");
+                string crearnom = Console.ReadLine();
+                banquer.CreaClient(crearnom);
+                break;
+            case "2":
+                Console.WriteLine("Ingrese el nombre del usuario existente");
+                string nomexistent = Console.ReadLine();
+                banquer.CreaClient(nomexistent);
+                Console.WriteLine(Client.Saldo);
+                Console.WriteLine("¿Quieres Ingresar (1) o Retirar (2)");
+                string IoR = Console.ReadLine();
+                switch (IoR)
+                {
+                    case "1":
+                        Console.WriteLine("¿Quanto quieres Ingresar?");
+                        int Ingresar = int.Parse(Console.ReadLine());
+                        Console.WriteLine(banquer.PosarDiners(nomexistent, Ingresar));
+                        Console.WriteLine(banquer.TreureDiners("Frederic", 200));
+                        break;
+                    case "2":
+                        Console.WriteLine("¿Quanto quieres Retirar?");
+                        int Retirar = int.Parse(Console.ReadLine());
+                        
+                        break;
+                }
+                break;
+            case "3":
+                break;
+            default:
+                Console.WriteLine("Me cago en tu puta madre");
+                break;
+        }
     }
 }
